e60c51d4372492623e8ee15d666bc2a3b674c163
Konrad Owsikowski <konrad.owsikowski@gmail.com>
2023-12-09 16:20:35 +0100

    Fixes for day 9

--- day9/solution.py
+++ day9/solution.py
@@ -2,19 +2,19 @@ import re
 
 
 def part_one(values_history: list[list[int]]) -> int:
-	sum_of_predictions = []
+	sum_of_predictions = 0
 	for value_history in values_history:
 		value_history_step = value_history
 		last_from_step = [value_history_step[-1]]
 		while set(value_history_step) != {0}:
 			value_history_step = [v2 - v1 for v1, v2 in zip(value_history_step[:-1], value_history_step[1:])]
 			last_from_step.append(value_history_step[-1])
-		sum_of_predictions.append(sum(last_from_step))
-	return sum(sum_of_predictions)
+		sum_of_predictions += sum(last_from_step)
+	return sum_of_predictions
 
 
 def part_two(values_history: list[list[int]]) -> int:
-	sum_of_predictions = []
+	sum_of_predictions = 0
 	for value_history in values_history:
 		value_history_step = value_history
 		first_from_step = [value_history_step[0]]
@@ -24,8 +24,8 @@ def part_two(values_history: list[list[int]]) -> int:
 		cur_pred_val = 0
 		for val in first_from_step[::-1]:
 			cur_pred_val = val - cur_pred_val
-		sum_of_predictions.append(cur_pred_val)
-	return sum(sum_of_predictions)
+		sum_of_predictions += cur_pred_val
+	return sum_of_predictions
 
 
 def parse_input() -> list[list[int]]:
