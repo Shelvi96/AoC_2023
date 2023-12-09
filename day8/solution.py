74a5cb677db76984be2271e2e0a63501f81b4d66
Konrad Owsikowski <konrad.owsikowski@gmail.com>
2023-12-09 16:17:01 +0100

    Fixes for day 8

--- day8/solution.py
+++ day8/solution.py
@@ -18,8 +18,8 @@ def parse_input_graph() -> Map:
 	with open("./input.txt", "r") as f:
 		lines = f.read()
 	instruction_regex = r"[LR]+"
-	instruction = re.search(instruction_regex, lines)[0]
 	network_regex = r"([0-9A-Z]{3}) = \(([0-9A-Z]{3}), ([0-9A-Z]{3})\)"
+	instruction = re.search(instruction_regex, lines)[0]
 	network = {
 		single_instruction.group(1): (single_instruction.group(2), single_instruction.group(3))
 		for single_instruction in re.finditer(network_regex, lines)
@@ -30,13 +30,15 @@ def parse_input_graph() -> Map:
 	)
 
 
-def part_one(input_map: Map, starting_node: str) -> int:
+def part_one(input_map: Map, starting_node: str, target_node: str) -> int:
 	current_node = starting_node
 	target_node = "ZZZ"
 	instruction_idx = 0
 	steps_count = 0
 	while current_node != target_node:
-		current_node = input_map.network[current_node][0] if input_map.instruction[instruction_idx] == "L" else input_map.network[current_node][1]
+		current_node = input_map.network[current_node][0] \
+			if input_map.instruction[instruction_idx] == "L" \
+			else input_map.network[current_node][1]
 		steps_count += 1
 		instruction_idx = (instruction_idx + 1) % len(input_map.instruction)
 	return steps_count
@@ -48,5 +50,5 @@ def part_two(input_map: Map) -> int:
 
 if __name__ == "__main__":
 	input_map: Map = parse_input_graph()
-	print(f"Part one: {part_one(input_map=input_map, starting_node='AAA')}")
+	print(f"Part one: {part_one(input_map=input_map, starting_node='AAA', target_node='ZZZ')}")
 	print(f"Part two: {part_two(input_map=input_map)}")
